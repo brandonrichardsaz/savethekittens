582c78d1c6ae952db438f3ba56aabf7a592e520e
Brandon Richards <brandonrichardsaz@gmail.com>
2017-04-07 07:23:13 -0700

    Initial Commit

---
+++ Save the KittensTests/Save_the_KittensTests.swift
@@ -0,0 +1,36 @@
+//
+//  Save_the_KittensTests.swift
+//  Save the KittensTests
+//
+//  Created by Richards, Brandon S. on 4/7/17.
+//  Copyright © 2017 Richards, Brandon S. All rights reserved.
+//
+
+import XCTest
+@testable import Save_the_Kittens
+
+class Save_the_KittensTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
